20b33676ae491deb5dca06681c45d0a9714f75c7
Yan Aung Hein <yaunghein@protonmail.com>
2021-07-18 21:08:11 +0630

    adding review slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,39 @@
+//Menu
 const menuBtn = document.getElementById('menu-btn');
 const navLinks = document.getElementById('nav-links');
 menuBtn.addEventListener('click', () => {
   navLinks.classList.toggle('open');
 });
 
+//Changing contact info based on screen size
 const contactLinks = document.querySelectorAll('.header__links--contact > .header__link-item > .header__link');
 window.addEventListener('resize', () => {
   if (window.innerWidth < 640) {
     contactLinks[1].textContent = 'Speak With Us';
+  } else {
+    contactLinks[1].textContent = 'yaunghein@protonmail.com';
   }
 });
-
 if (window.innerWidth < 640) {
   contactLinks[1].textContent = 'Speak With Us';
 }
+
+//Review Slider
+const swiper = new Swiper('.swiper-container', {
+  speed: 400,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
