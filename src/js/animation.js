e0435d04c2639c7bd006419c0dc0b0454fb4a5f4
Yan Aung Hein <yaunghein@protonmail.com>
2021-08-09 08:03:07 +0630

    AOS

---
+++ src/js/animation.js
@@ -0,0 +1,17 @@
+const heroTl = gsap.timeline();
+const duration = 0.8;
+const ease = 'Power2.easeInOut';
+heroTl.from('.sub-title', { duration, y: 16, opacity: 0, ease });
+heroTl.from('.hero__title', { duration, y: 16, opacity: 0, ease }, '-=0.6');
+heroTl.from('.hero__p', { duration, y: 16, opacity: 0, ease }, '-=0.6');
+heroTl.from('.buttons-wrapper', { duration, y: 16, opacity: 0, ease }, '-=0.6');
+// heroTl.from('.hero__button-2', { duration, y: 16, opacity: 0, ease }, '-=0.25');
+heroTl.from('.hero__image-wrapper', { duration, scale: 0.8, opacity: 0, ease }, '-=0.8');
+heroTl.from('.header__logo', { duration, y: 16, opacity: 0, ease, stagger: 0.1 }, '-=0.6');
+heroTl.from('.header__link-item', { duration, y: 16, opacity: 0, ease, stagger: 0.05 }, '-=0.6');
+
+AOS.init({
+  offset: 120, // offset (in px) from the original trigger point
+  duration: 800, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+});
