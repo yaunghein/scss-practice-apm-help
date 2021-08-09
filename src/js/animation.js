6da1605c532b4118e6e8c6ac8b64ef7054d9e555
Yan Aung Hein <yaunghein@protonmail.com>
2021-08-09 15:22:50 +0630

    desktop dropdown fixed

--- src/js/animation.js
+++ src/js/animation.js
@@ -6,6 +6,8 @@ heroTl.from('.hero__title', { duration, y: 16, opacity: 0, ease }, '-=0.6');
 heroTl.from('.hero__p', { duration, y: 16, opacity: 0, ease }, '-=0.6');
 heroTl.from('.buttons-wrapper', { duration, y: 16, opacity: 0, ease }, '-=0.6');
 heroTl.from('.hero__image-wrapper', { duration, scale: 0.8, opacity: 0, ease }, '-=0.8');
+heroTl.from('.header__logo', { duration, opacity: 0, ease }, '-=0.8');
+heroTl.from('.header__link', { duration, opacity: 0, ease, stagger: '0.1' }, '-=0.8');
 
 AOS.init({
   offset: 120, // offset (in px) from the original trigger point
